5f749558636184c9f076b5251f9c639f8f236248
loembetyannick <loembetyannick@icloud.com>
2023-12-04 01:21:52 +0000

    adding validation

--- EcommerceAPI/EcommerceAPI/Services/Concrete/ShopService.cs
+++ EcommerceAPI/EcommerceAPI/Services/Concrete/ShopService.cs
@@ -27,20 +27,61 @@ namespace EcommerceAPI.Services.Concrete
 
         public async Task<ProductDto> GetProduct(int id)
         {
+            if (id <= 0)
+            {
+                throw new ArgumentException("Invalid product ID.", nameof(id));
+            }
             var result = await _context.Products.FindAsync(id);
+
+            if (result == null)
+            {
+                throw new ArgumentNullException($"Product with ID {id} not found.", nameof(id));
+            }
+
             var product = _mapper.Map<ProductDto>(result);
             return product;
         }
 
         public PurchaseDto Checkout(BasketDto basket)
         {
+            if(basket == null)
+            {
+                throw new ArgumentNullException(nameof(basket), "Basket cannot be null.");
+            }
+
+            if(basket.UserId <= 0)
+            {
+                throw new ArgumentException("Invalid user ID in the basket.", nameof(basket.UserId));
+            }
+
+            if (basket.ProductIds == null || !basket.ProductIds.Any())
+            {
+                throw new ArgumentException("No products in the basket.", nameof(basket.ProductIds));
+            }
+
             var products = new List<ProductDto>();
 
-            var productsList = _context.Products.ToList();
+            var productIds = basket.ProductIds.Select(item => item.ItemId);
+            var productsList = _context.Products.Where(p => productIds.Contains(p.Id)).ToList();
 
-            foreach(ItemDto item in basket.ProductIds)
+            var invalidProductIds = basket.ProductIds
+                .Select(item => item.ItemId)
+                .Except(productsList.Select(product => product.Id))
+                .ToList();
+
+            if (invalidProductIds.Any())
+            {
+                throw new ArgumentException($"Invalid product IDs: {string.Join(", ", invalidProductIds)}", nameof(basket.ProductIds));
+            }
+
+            foreach (ItemDto item in basket.ProductIds)
             {
                 var result = _context.Products.First(x => x.Id == item.ItemId);
+
+                if (item.Quantity <= 0)
+                {
+                    throw new ArgumentException($"Invalid quantity for product ID {item.ItemId}: {item.Quantity}", nameof(item.Quantity));
+                }
 
                 result.Quantity = item.Quantity;
                 result.Price = result.Quantity * result.Price;
